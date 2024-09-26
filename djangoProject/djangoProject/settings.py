2bb17005f3f7b92a41ea9338756536586e9647d5
zeyad-shaban <zeyadshapan2004@gmail.com>
2024-09-26 20:52:55 +0300

    added .gitignore and configurtaion for pycharm

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-_#9u)!1+=m&8hmgd5lrr69eubr0vry86&(=ro=u#=0wdcwi062
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['RobenScheduleOrganizer.pythonanywhere.com', 'localhost']
 
 
 # Application definition
@@ -118,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
